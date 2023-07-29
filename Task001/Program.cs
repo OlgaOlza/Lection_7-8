8df8cc81a0616d1558bb2c71924603bb6eb35615
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-29 14:44:10 +0300

    Собрать строку с числами от а до b

---
+++ Task001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Собрать строку с числами от а до b, a <= и
+// Рекурсия
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
