4c26cd73495be989c659eb73bb5ca54c0dc1dc68
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-29 15:53:01 +0300

    Факториал числа. Рекурсия

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -2,14 +2,14 @@
 
 int FactorialFor(int n)
 {
-int result = 1;
-for (int i = 0; i <= n; i++) result *= i;
-return result;
+    int result = 1;
+    for (int i = 0; i <= n; i++) result *= i;
+    return result;
 }
 int FactorialRec(int n)
 {
-if (n == 1) return 1;
-else return n * FactorialRec(n - 1);
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
 }
 
 Console.WriteLine(FactorialFor(10));
