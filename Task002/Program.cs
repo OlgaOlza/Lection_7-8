ba4c8679c06d925c29f04c28330bd9474c78a02d
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-29 14:50:03 +0300

    числа от большего к меньшему Рекурсия

---
+++ Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Собрать строку с числами от а до b, a >= и
+// Рекурсия
+
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
