a19807beec0517d8320c7ad677c0e4d94c6b8ab8
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-29 15:55:54 +0300

    Сумма чисел от 1 до n

--- Tsk003/Program.cs
+++ Tsk003/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Сумма чисел от 1 до n
 // Рекурсия 
 
-int SumFor (int n)
+int SumFor(int n)
 {
     int result = 0;
     for (int i = 0; i <= n; i++) result += i;
     return result;
 }
-int SumRec (int n)
+int SumRec(int n)
 {
-if (n == 0) return 0;
-else return n + SumRec(n-1);
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
 }
 
 Console.WriteLine(SumFor(10));
